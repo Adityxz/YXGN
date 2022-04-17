db57e69aabba1a1a38d5925d046a944bfa271067
Adityxz <tiyo2003@gmail.com>
2022-04-17 19:28:05 +0700

    wtf

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const btns = document.querySelectorAll(".nav-btn");
 const slides = document.querySelectorAll(".image-slide");
-const contents = document.querySelectorAll(".content");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
 var sliderNav = function (manual) {
   btns.forEach((btn) => {
     btn.classList.remove("active");
